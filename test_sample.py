f5aed7ac09431d1a6a342dabff8c0920eaeabde2
ashishm641 <48187685+ashishm641@users.noreply.github.com>
2023-07-30 23:58:40 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -12,19 +12,15 @@ def driver(request):
     base_url = "https://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/wd/hub"
 
     # Create the Remote WebDriver with the specified capabilities
-    # Create the Remote WebDriver with the specified capabilities
-    driver = webdriver.Remote(command_executor=base_url, capabilities=capabilities)
+    driver = webdriver.Remote(command_executor=base_url, desired_capabilities=capabilities)
     
-
-    # Add teardown code to close the browser after the test is done
     def fin():
         driver.quit()
+    
     request.addfinalizer(fin)
-
     return driver
 
-# Your test function using the driver fixture
 def test_example(driver):
-    # Your test code using the driver fixture
-    driver.get('https://example.com')
+    # Your test code goes here
+    driver.get("https://www.example.com")
     assert "Example Domain" in driver.title
