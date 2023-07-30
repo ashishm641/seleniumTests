cfefcecb113ca21739bf7654a1a79ec977caa09d
ashishm641 <48187685+ashishm641@users.noreply.github.com>
2023-07-31 00:04:17 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 @pytest.fixture
 def driver(request):
     # Define the desired capabilities for Selenium Grid
-    capabilities = DesiredCapabilities.CHROME.copy()
-    capabilities['browserName'] = 'chrome'
-    capabilities['version'] = 'latest'
-    capabilities['platform'] = 'macOS 11.0'
-    capabilities['acceptInsecureCerts'] = True
+    capabilities = {
+        "browserName": "chrome",
+        "version": "latest",
+        "platform": "macOS 11.0",
+        "acceptInsecureCerts": True
+    }
     base_url = "https://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/wd/hub"
 
     # Create the Remote WebDriver with the specified capabilities
