b0b5bedd7c66a6c95f334d1b135d905ff2ba3e16
ashishm641 <48187685+ashishm641@users.noreply.github.com>
2023-07-31 08:18:18 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,32 +1,35 @@
-import unittest
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class TestSeleniumGrid(unittest.TestCase):
+@pytest.fixture
+def driver(request):
+    # Define the desired capabilities for Chrome
+    options = webdriver.ChromeOptions()
+    options.browser_name = "chrome"
+    options.version = "latest"
+    options.platform = "macOS 11.0"
+    # The following line will tell Selenium to accept connections to servers that are using self-signed certificates or outdated versions of SSL.
+    options.ssl_version = "AUTO"
+    base_url = "http://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/wd/hub"
 
-    def setUp(self):
-        # Define the desired capabilities for Chrome
-        options = webdriver.ChromeOptions()
-        options.browser_name = "chrome"
-        options.version = "latest"
-        options.platform = "macOS 11.0"
-        base_url = "https://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/wd/hub"
+    # Create the Remote WebDriver with the specified capabilities
+    driver = webdriver.Remote(command_executor=base_url, options=options)
 
+    def fin():
+        driver.quit()
 
-        # Create the Remote WebDriver with the specified capabilities
-        self.driver = webdriver.Remote(command_executor=base_url, options=options)
+    request.addfinalizer(fin)
+    return driver
 
-    def test_example(self):
-        # Your test code goes here
-        self.driver.get("https://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/wd/hub")
-        assert "Selenium Grid" in self.driver.title
-        self.driver.find_element_by_id("username").send_keys("admin")
-        self.driver.find_element_by_id("password").send_keys("admin")
-        self.driver.find_element_by_id("submit").click()
-        assert "Dashboard" in self.driver.title
-
-    def tearDown(self):
-        self.driver.quit()
+def test_example(driver):
+    # Your test code goes here
+    driver.get("http://a9a62f8fea028418b89cc1c7fae3c67f-808020604.us-east-1.elb.amazonaws.com:4444/ui#/sessions")
+    assert "Selenium Grid" in driver.title
+    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_id("password").send_keys("admin")
+    driver.find_element_by_id("submit").click()
+    assert "Dashboard" in driver.title
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
